8955d979921e6a5a050070cbaec5e57cc78ee551
Kyle <67796894+Chaos-Consolidated@users.noreply.github.com>
2025-03-17 19:11:25 +0000

    asd

--- setup.sh
+++ setup.sh
@@ -6,4 +6,17 @@ clear
 echo "Shell updated..."
 
 # Install/update python
-apt install -y python3.12
+apt install -y python
+clear
+pyversion=$(python --version)
+echo "$pyversion installed!"
+clear
+
+# Install pip
+apt install -y python3-pip
+clear
+
+# Make requirements file
+touch requirements.txt
+
+# Make project files
