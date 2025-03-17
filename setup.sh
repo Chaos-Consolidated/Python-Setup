ca2662e96bd9473fc15459776578ca6a9d39739b
Kyle <67796894+Chaos-Consolidated@users.noreply.github.com>
2025-03-17 19:46:02 +0000

    Initial idea complete

--- setup.sh
+++ setup.sh
@@ -20,3 +20,28 @@ clear
 touch requirements.txt
 
 # Make project files
+mkdir src
+mkdir docs
+mkdir tests
+
+# Setup main.py
+echo -e "\"\"\"" > src/main.py
+echo -e "   Title:" >> src/main.py
+echo -e "   Description:" >> src/main.py
+echo -e "\n" >> src/main.py    
+echo -e "   Updated By: <NAME>" >> src/main.py
+echo -e "   Date: <DATE>" >> src/main.py
+echo -e "\n" >> src/main.py   
+echo -e "   Algorithm steps:" >> src/main.py 
+echo -e "\n" >> src/main.py   
+echo -e "\"\"\"" >> src/main.py
+echo -e "\n" >> src/main.py
+echo -e "import os" >> src/main.py
+echo -e "\n" >> src/main.py
+echo -e "os.system('python -m pip install --upgrade pip')" >> src/main.py
+echo -e "os.system('python -m pip install -r requirements.txt')" >> src/main.py
+
+# Init pip installs
+python3 src/main.py
+clear
+echo "Setup complete!"
